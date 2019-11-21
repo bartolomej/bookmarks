9d4726025dec9094818e832850688bd717b6acdb
bartolomej <bartolomej.kozorog@gmail.com>
2019-11-21 08:37:38 +0100

    Updates

---
+++ precache-manifest.2dbde86b2ee5c1e4c721c8d1fc1b9b52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0236045b6c2d0e7ae9b1c58547f71ace",
+    "url": "/cool-links/index.html"
+  },
+  {
+    "revision": "75dd9f02d5b716eb94f9",
+    "url": "/cool-links/static/css/main.a39333f6.chunk.css"
+  },
+  {
+    "revision": "98ba7d7526bcc36d1158",
+    "url": "/cool-links/static/js/2.2c897260.chunk.js"
+  },
+  {
+    "revision": "75dd9f02d5b716eb94f9",
+    "url": "/cool-links/static/js/main.207be752.chunk.js"
+  },
+  {
+    "revision": "bfeb79312a842fab75b3",
+    "url": "/cool-links/static/js/runtime-main.3a7439c2.js"
+  }
+]);
